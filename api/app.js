72f7b7302e893579a298e58715850dbc5c2d4077
Antonio69999 <antoine.chabeauti@gmail.com>
2024-04-18 11:06:18 +0200

    Remove client from git index

--- api/app.js
+++ api/app.js
@@ -2,11 +2,18 @@ import express from "express";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
