214e6d2cb7e69506f0230b7e6587aea71be3aad9
Antonio69999 <antoine.chabeauti@gmail.com>
2024-04-24 15:04:05 +0200

    Add client code

---
+++ client/src/lib/loader.js
@@ -0,0 +1,6 @@
+import apiRequest from "../lib/apiRequest.js";
+
+export const singlePageLoader = async ({ request, params }) => {
+  const res = await apiRequest("/posts/"+ params.id);
+  return res.data;
+};
